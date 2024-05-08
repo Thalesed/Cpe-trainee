61401124e40265423008bf3b6c240f44f1775f3b
Thalesed <thalesdeduardos@gmail.com>
2024-05-08 16:15:46 +0000

    08/05 Prototipo inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Teste</h1>
+      <Home />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/pages/Header.js
@@ -0,0 +1,18 @@
+// src/components/Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>CPE</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
