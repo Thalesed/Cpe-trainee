5eeff70cf107d2ae50cc25d31ed51041e17955d6
Thalesed <thalesdeduardos@gmail.com>
2024-05-10 16:29:58 +0000

    10/05 carrossel responsividade

--- src/components/carrossel/style.js
+++ src/components/carrossel/style.js
@@ -1,21 +1,26 @@
 import styled from "styled-components";
 
 export const SlideBox = styled.div`
-    width: 50%; 
-    margin: 8px auto;
-    .slick-dots li.slick-active button:before {
-    color: #ffffff; /* Cor do botão ativo */
+  width: 30%;
+  margin: 8px auto;
+  @media (max-width: 768px) {
+    width: 70%; 
   }
-  .slideBox img{
-    width: 50%; /* Define a largura da div */
+  .slick-dots li.slick-active button:before {
+    color: #ffffff;
+  }
+  .slideBox img {
+    width: 80%;
+    display: block;
     margin: 0 auto;
+  }
+  @media (max-width: 768px) {
+    width: 70%; 
+    .slideBox img {
+    width: 100%;
     }
-    .slick-dots li button:before {
-        color: rgba(190, 182, 182, 0.932); /* Cor dos pontos */
-    }
-`;
-
-
-export const TabelaH = styled.h3`
-   margin-left: 0 auto;
+  }
+  .slick-dots li button:before {
+    color: rgba(190, 182, 182, 0.932);
+  }
 `;
--- src/components/tabela/style.js
+++ src/components/tabela/style.js
@@ -1,25 +1,37 @@
 import styled from "styled-components";
 
 export const TabelaBox = styled.div`
-    width: 20%;
-    margin: 50px auto;
-    border-radius: 27px;
-    min-height: 100px;
-    min-width: 600px;
-    border: 2px solid #000000;
-    background-color: rgb(77, 71, 71);
-    overflow: hidden;
-    .TabelaBox .titulo{
-        color: black;
-        background-color: gold;
-        width: auto;
-        display: flex;
-        justify-content: space-between;
-        padding: 0 50px;
-    }
+  width: 80%; 
+  max-width: 600px; 
+  margin: 50px auto;
+  border-radius: 27px;
+  min-height: 100px;
+  border: 2px solid #000000;
+  background-color: rgb(77, 71, 71);
+  overflow: hidden;
+  
+  @media (max-width: 768px) {
+    width: 90%; 
+  }
 
-    .TabelaBox .titulo h3{
-        margin-left: 0 auto;
-    }
+  .titulo {
+    color: black;
+    background-color: gold;
+    width: auto;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 20px; 
+  }
+  @media (max-width: 768px) {
+    .titulo {
+        max-height: 50px;
+    } 
+  }
+
+
+  .titulo h3 {
+    margin-left: 0;
+  }
 `;
+
 
