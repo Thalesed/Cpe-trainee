8864f50eb0eb5bbf8a09f192e7df7be5115fc2b4
TheoryMan <gustavo.olicardoso07@gmail.com>
2024-05-22 00:48:03 -0300

    Front Login Design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,36 +1,42 @@
  import React from "react"
-import Input from "../../components/Input/Input"
 import TituloLogin from "../../components/titulo/tituloLogin"
-import BotaoLogin from "../../components/botão/botaoLogin"
 import { useNavigate } from "react-router-dom";
-import { Container, HighlightLink } from "./style";
+import { Container, Container2, HighlightLink, Campo, Input, Button } from "./style";
+import { useState } from "react";
 
-const Login = () =>{
 
+
+export default function Login () {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
     const navigate = useNavigate();
+    const handleSubmit = async (e) => { 
+        e.preventDefault();
+        console.log({email, senha});
+     }
     return (
-   
-       
-        <>
+     <Container2>
         <TituloLogin tituloStr="Login"/>
-        <Input x='E-mail'/>
-        <Input x='Senha'/>
-        <divt>
-       <Container>
-           <p>
-            Ainda não tem uma conta? Faça seu cadastro  <HighlightLink className="aquiStyled" onClick= {() => navigate("/cadastro")}>aqui</HighlightLink>
-          <BotaoLogin onClick={() => navigate("/")}>  </BotaoLogin>
-
-          </p> 
-
           
-          </Container>
-        </divt>
-       
+           <Container>
+             <form onSubmit={handleSubmit}>
+              <Campo>
+                <Input placeholder="Email" type="email" name="email" id= "email" required onChange={(e) => setEmail(e.target.value)}/>
+              </Campo>
+              <Campo>           
+                <Input placeholder="Senha" type="password" name="senha" id="senha" required onChange={(e) => setSenha(e.target.value)}/>
+              </Campo>               
+                 
+            <p>
+            Ainda não tem uma conta? Faça seu cadastro  <HighlightLink className="aquiStyled" onClick= {() => navigate("/cadastro")}>aqui</HighlightLink>       
+            </p>   
 
-      
-       
-        </>
+               <Button type="submit">Entrar</Button>
+             </form> 
+            
+           </Container>
+        
+         </Container2>
     
 
 
@@ -38,4 +44,3 @@ const Login = () =>{
     )
 }
 
-export default Login;
