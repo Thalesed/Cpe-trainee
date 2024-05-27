b379977c49b1e591e73ebb93a29c732999fc663a
TheoryMan <gustavo.olicardoso07@gmail.com>
2024-05-27 19:11:56 -0300

    Linkagem Cadastro completa

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { DivInput, InputStyle, TituloC } from '../../pages/Cadastro/style';
 
-const Input = ({x}) => {
+const Input = ({x, id, register, error}) => {
     return (
         <>
         <DivInput>
-            <InputStyle type='text' placeholder={x}/>
+            <InputStyle type='text' placeholder={x} id={id} error= {error} {...register(id)}/>
         </DivInput>
         </>
     );
--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -2,6 +2,10 @@ import api from "./api";
 
 
 export async function getCadastro (){
-    const{data} = await api.get("/cadastro");
+    const{data} = await api.get("/usuarios");
+    return data;
+}
+export async function CreateCadastro (campos){
+    const{data} = await api.post("/usuarios", campos);
     return data;
 }
