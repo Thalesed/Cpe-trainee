688a2f33e5d69e160a57190cc46f89226b550692
Guilherme Fachinelli <Facnelli@gmail.com>
2024-06-01 02:23:25 -0300

    adicionei o icone de voltar no header de algumas páginas

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,11 +7,14 @@ import {
 import { AppLayout } from "./components";
 import { Home, Cadastro, Login, EditarUsuario } from "./pages";
 import Projetos from "./pages/Projetos/Projetos";
+import { AppVoltar } from "./components/AppLayout/AppLayout";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Home />} />
+      </Route>
+      <Route path="/" element={<AppVoltar />}>
         <Route path="cadastro" element={<Cadastro />} />
         <Route path="login" element={<Login />} />
         <Route path="editar" element={<EditarUsuario />} />
