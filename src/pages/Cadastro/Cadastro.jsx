51fba90d64cb444aa15aedc555bfe5b4f80ca0a1
TheoryMan <gustavo.olicardoso07@gmail.com>
2024-06-03 10:33:00 -0300

    Resolvendo Conflitos

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import Input from "../../components/Input/Input"
-import TituloCadastro from "../../components/titulo/titulo"
-import BotaoCadastro from "../../components/botão/botãoCadastro"
+import React from "react";
+import Input from "../../components/Input/Input";
+import TituloCadastro from "../../components/titulo/titulo";
+import BotaoCadastro from "../../components/botão/botãoCadastro";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import BotaoVoltarCadastro from "../../components/botão/botaoVoltar"
+import BotaoVoltarCadastro from "../../components/botão/botaoVoltar";
 //import FundoCadastro from "../../components/fundo/fundoCadastro"
 import api from "../../services/api";
 import { useGetCadastro } from "../../Hooks/query/Tools";
@@ -12,38 +12,62 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { validator } from "./utils";
 
-export default function Cadastro () {
-    const {data: cadastro } = useGetCadastro({
-        onError: (err) => {
-            console.log(err);
-        }
-    })
-    const { handleSubmit, register, formState: {errors}, } = useForm({resolver: zodResolver(validator)});
-    function onSubmit(data){
-        console.log(data);
-    }
-     
-    return(
-        <>
-        <TituloCadastro tituloStr="cadastrar"/>
-        <form onSubmit={handleSubmit(onSubmit)}>
-           
+export default function Cadastro() {
+  const { data: cadastro } = useGetCadastro({
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(validator) });
+  function onSubmit(data) {
+    console.log(data);
+  }
 
-          
-                   
-        <Input register = {register} error={!!errors?.email?.message} x="Email" id= "email" />
+  return (
+    <>
+      <TituloCadastro tituloStr="cadastrar" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          register={register}
+          error={!!errors?.email?.message}
+          x="Email"
+          id="email"
+        />
         {errors?.email?.message && <p>{errors?.email?.message}</p>}
-        <Input register = {register} error={!!errors?.senha?.message} x="Senha"  id="senha" />
+        <Input
+          register={register}
+          error={!!errors?.senha?.message}
+          x="Senha"
+          id="senha"
+        />
         {errors?.senha?.message && <p>{errors?.senha?.message}</p>}
-        <Input register = {register} error={!!errors?.nome?.message}x="Nome"  id="nome" />
+        <Input
+          register={register}
+          error={!!errors?.nome?.message}
+          x="Nome"
+          id="nome"
+        />
         {errors?.nome?.message && <p>{errors?.nome?.message}</p>}
-        <Input register = {register}error={!!errors?.cargo?.message} x="Cargo"  id="cargo" />
+        <Input
+          register={register}
+          error={!!errors?.cargo?.message}
+          x="Cargo"
+          id="cargo"
+        />
         {errors?.cargo?.message && <p>{errors?.cargo?.message}</p>}
-        <Input register = {register}error={!!errors?.status?.message} x="Status"  id="status"/>
+        <Input
+          register={register}
+          error={!!errors?.status?.message}
+          x="Status"
+          id="status"
+        />
         {errors?.status?.message && <p>{errors?.status?.message}</p>}
-        <BotaoCadastro type="submit"/>
-       </form>
-        </>
-    )
-   
+        <BotaoCadastro type="submit" />
+      </form>
+    </>
+  );
 }
