5462d7c02afc377431024a3fa7095a2c4a54ce88
Guilherme Fachinelli <Facnelli@gmail.com>
2024-06-04 04:19:06 -0300

    Modal delete projetos feito

--- src/Services/api/endpoints.js
+++ src/Services/api/endpoints.js
@@ -1,5 +1,6 @@
 import api from "./api";
 
+//Login
 export async function getLogin() {
   const { data } = await api.get("/usuarios");
   return data;
@@ -11,17 +12,33 @@ export async function postLogin(body) {
 }
 
 export async function deleteLogin(id) {
-  const { data } = await api.post(`/login/${id}`);
+  const { data } = await api.delete(`/login/${id}`);
   return data;
 }
 
-
-export async function getUsuarios(){
-  const {data} = await api.get('/usuarios');
+//Usuarios
+export async function getUsuarios() {
+  const { data } = await api.get("/usuarios");
   return data;
 }
 
-export async function updateUsuario(id, usuarioAtualizado){
-  const {data} = await api.put(`/usuarios/${id}`, usuarioAtualizado);
+export async function updateUsuario(id, usuarioAtualizado) {
+  const { data } = await api.put(`/usuarios/${id}`, usuarioAtualizado);
+  return data;
+}
+
+//Projetos
+export async function getProjetos() {
+  const { data } = await api.get("/projeto");
+  return data;
+}
+
+export async function deleteProjetos(id) {
+  const { data } = await api.delete(`/projeto/${id}`);
+  return data;
+}
+
+export async function postProjetos(body) {
+  const { data } = await api.post("/projeto", body);
   return data;
 }
